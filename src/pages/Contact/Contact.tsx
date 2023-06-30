9159fe4737fe8665ba9ac647ebae7eebccdcda1a
Samantha Oh <sami_oh@berkeley.edu>
2023-06-30 16:52:19 -0700

    fixed clipboard

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -23,37 +23,40 @@ const Contact = () => {
             <Typography variant="h2" className={styles.smallpadding}>
               Contact Me!
             </Typography>
-            <Stack direction={"row"} spacing={"2%"}>
+            <div className={styles.contactinfo}>
               <Typography variant="body2">
                 <span style={{ fontWeight: "bolder" }}>Email:</span>{" "}
                 sami_oh@berkeley.edu
               </Typography>
               <CopyToClipboardButton text="sami_oh@berkeley.edu" />
-            </Stack>
-            <Stack direction={"row"} spacing={"2%"}>
+            </div>
+            <div className={styles.contactinfo}>
               <Typography variant="body2">
                 <span style={{ fontWeight: "bolder" }}>Mobile:</span>{" "}
                 714-321-7505
               </Typography>
               <CopyToClipboardButton text="714-321-7505" />
-            </Stack>
-
-            <a
-              className={styles.link2}
-              href="https://www.linkedin.com/in/samantha-oh-3315b31b7/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Typography variant="body2">LinkedIn</Typography>
-            </a>
-            <a
-              className={styles.link2}
-              href="https://drive.google.com/file/d/1VE-5CvzKy00ap0mXMIRyEHI3X2yMoG3K/view?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Typography variant="body2">Resume</Typography>
-            </a>
+            </div>
+            <div>
+              <a
+                className={styles.link2}
+                href="https://www.linkedin.com/in/samantha-oh-3315b31b7/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Typography variant="body2">LinkedIn</Typography>
+              </a>
+            </div>
+            <div>
+              <a
+                className={styles.link2}
+                href="https://drive.google.com/file/d/1VE-5CvzKy00ap0mXMIRyEHI3X2yMoG3K/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Typography variant="body2">Resume</Typography>
+              </a>
+            </div>
           </Stack>
         </Stack>
       </div>
