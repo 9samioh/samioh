5d20f561c6bacbce170334c38996a11e606836e9
Samantha Oh <sami_oh@berkeley.edu>
2023-06-30 12:42:02 -0700

    changed to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Work from "./pages/Work/Work";
 import About from "./pages/About/About";
 import Art from "./pages/Art/Art";
